13f3e9d1f4585224c51ecf2f2795359b85e2ac97
Ivan Enzhaev <8observer8@gmail.com>
2025-01-08 16:31:10 +0400

    Add new lines with ^

--- config-web.bat
+++ config-web.bat
@@ -1,1 +1,3 @@
-emcmake cmake -G "MinGW Makefiles" -S . -B dist/web -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/web/lib/cmake/SDL3 -DCMAKE_BUILD_TYPE=Debug
+emcmake cmake -G "MinGW Makefiles" -S . -B dist/web ^
+-DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/web/lib/cmake/SDL3 ^
+-DCMAKE_BUILD_TYPE=Debug
